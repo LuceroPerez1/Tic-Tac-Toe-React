5e03d91cc05ef8f9dc1cd1e310226e81eeee63ed
dcalle <duvancalle7@gmail.com>
2023-10-05 19:58:07 -0500

    Add loops for the board and styles when there is a winner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import confetti from "canvas-confetti";
+// import confetti from "canvas-confetti";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, className }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${className}`} onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, currentMove }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -23,33 +23,44 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  const winnerInfo = calculateWinner(squares);
+  const winnerLine = winnerInfo ? winnerInfo.line : null;
+
+  function renderSquare(index, isWinnerSquare) {
+    return (
+      <Square
+        key={index}
+        value={squares[index]}
+        onSquareClick={() => handleClick(index)}
+        className={isWinnerSquare ? 'winner-square' : ''}
+      />
+    );
+  }
+
+  const board = [0, 1, 2].map((row) => (
+    <div className="board-row" key={row}>
+      {[0, 1, 2].map((col) => {
+        const index = row * 3 + col;
+        const isWinnerSquare = winnerLine && winnerLine.includes(index);
+        return renderSquare(index, isWinnerSquare);
+      })}
+    </div>
+  ));
+
   let status;
-  if (winner) {
-    status = "Winner: " + winner;
-    confetti()
+  if (winnerInfo) {
+    status = "Winner: " + winnerInfo.winner;
+    // confetti()
+  } else if (currentMove === 9) {
+    status = "It's a draw!";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div className="status">{status}</div >
+      <div>{board}</div>
     </>
   );
 }
@@ -86,7 +97,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} currentMove={currentMove} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -109,7 +120,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: [a, b, c] };
     }
   }
   return null;
